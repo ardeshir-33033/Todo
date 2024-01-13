c5035955f1620e2d616bd37d33a10291e7fbb47b
ardeshir <ardeshirojan@gmai.com>
2024-01-13 22:15:47 +0330

    added datasource unit tests

---
+++ test/unit_test/features/map/data/data_sources/todo_list_data_source_test.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:map_ws/core/base/environment.dart';
+import 'package:map_ws/features/map/data/data_sources/location_data_source.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import '../../../../../fixtures/t_location_model.dart';
+import 'location_data_source_test.mocks.dart';
+
+@GenerateMocks([TodoListDataSource])
+void main() {
+  late TodoListDataSource dataSource;
+
+  setUp(() async {
+    await Environment.initEnvironment();
+    dataSource = MockTodoListDataSource();
+  });
+
+  group('get locations data source', () {
+    final tLocationList = [TestModels.testModelForLocation];
+
+    final StreamController<dynamic> testDataController =
+        StreamController<dynamic>();
+    testDataController.add(tLocationList);
+
+    test(
+      'should return dynamic stream when get location is successful',
+      () async {
+        //arrange
+        when(dataSource.getLocations())
+            .thenAnswer((realInvocation) => testDataController.stream);
+        //act
+        final result = dataSource.getLocations();
+        //assert
+        verify(dataSource.getLocations());
+        expect(result, equals(testDataController.stream));
+      },
+    );
+
+    test(
+      'should return Exception when get location is not successful',
+      () async {
+        //arrange
+        when(dataSource.getLocations()).thenThrow(Exception());
+        //act
+        final call = dataSource.getLocations;
+        //assert
+        expect(() => call(), throwsA(const TypeMatcher<Exception>()));
+      },
+    );
+  });
+}
