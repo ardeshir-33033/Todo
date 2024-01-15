c70ad792a068bcd2cc405aa9663df92adb445f4b
ardeshir <ardeshirojan@gmai.com>
2024-01-15 05:39:45 +0330

    fixed repository tests

---
+++ test/unit_test/features/map/data/repositories/todo_repository_impl_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:todo_list/core/base_routing/environment.dart';
+import 'package:todo_list/features/todo/data/repositories/todo_repository_impl.dart';
+import '../../../../../fixtures/t_todo_model.dart';
+import '../data_sources/todo_list_data_source_test.mocks.dart';
+
+void main() {
+  late MockTodoDataSource mockTodoDataSource;
+  late TodoRepositoryImpl repository;
+
+  setUp(() async {
+    await Environment.initEnvironment();
+
+    mockTodoDataSource = MockTodoDataSource();
+    repository = TodoRepositoryImpl(mockTodoDataSource);
+  });
+
+  group('get todo list', () {
+    final tTodoList = [TestModels.testModelForTodoItem];
+
+    test(
+      'should return UserModel when create user is successful',
+      () async {
+        //arrange
+        when(mockTodoDataSource.getTodoItems())
+            .thenAnswer((realInvocation) async => tTodoList);
+        //act
+        final result = await repository.getTodoList();
+        //assert
+        verify(mockTodoDataSource.getTodoItems());
+        expect(result, equals(tTodoList));
+      },
+    );
+
+    test(
+      'should return Exception when get todo is not successful',
+      () async {
+        //arrange
+        when(mockTodoDataSource.getTodoItems()).thenThrow(Exception(""));
+        //act
+        final call = repository.getTodoList;
+        //assert
+        expect(() => call(), throwsA(const TypeMatcher<Exception>()));
+      },
+    );
+  });
+}
